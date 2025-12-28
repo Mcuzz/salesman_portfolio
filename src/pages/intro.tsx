d3f2915bf3cb4d4ce05f4aabd7d8a11d4fb37cc3
natvy <natvyl4@gmail.com>
2025-12-27 21:02:20 -0700

    correccion de intro ruta

--- src/pages/intro.tsx
+++ src/pages/intro.tsx
@@ -1,5 +1,10 @@
+import { useRouter } from "next/router";
 import IntroPage from "@/components/intro";
 
 export default function Intro() {
-  return <IntroPage />;
+  const router = useRouter();
+
+  return (
+    <IntroPage onFinish={() => router.push("/")} />
+  );
 }
